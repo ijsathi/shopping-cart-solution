cef7014a5cbecf0f54c2010da37851b605f579be
ijsathi <ijsathi2001rs@gmail.com>
2021-08-18 15:45:23 +0600

    all done

--- app.js
+++ app.js
@@ -10,8 +10,29 @@ function updateProductNumber (product, price, isIncreasing){
     productInput.value=productNumber;
     // update case total
     const productTotal = document.getElementById(product + '-total')
-    productTotal.innerText = productNumber * price;   
+    productTotal.innerText = productNumber * price; 
+    // calvulate total
+    calculateTotal(); 
 }
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+    
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-amount').innerText=tax;
+    document.getElementById('total-price').innerText=totalPrice;  
+}
+
 // phone Increase decrease events
 document.getElementById('phone-plus').addEventListener('click', function(){
     
